7257c5bb3ca5b13f3ac3f0fd5c3613f10858893f
Mohammad El-Shennawy <mohamedwork216@gmail.com>
2026-08-05 00:23:44 +0300

    keep a small buffer per connection and only process complete commands ending in \r\n

--- app/main.js
+++ app/main.js
@@ -1,17 +1,27 @@
 const net = require("net");
 
-console.log("Logs from your program will appear here!");
-
 const server = net.createServer((connection) => {
+    let buffer = "";
     connection.on("data", (data) => {
-        connection.write("+PONG\r\n");
+        buffer += data.toString();
+        let index;
+        while ((index = buffer.indexOf("\r\n")) != -1) {
+            const command = buffer.slice(0, index);
+            buffer = buffer.slice(index + 2);
+            console.log("Received command:", command);
+            if (command === "PING") {
+                connection.write("+PONG\r\n");
+            }
+        }
         console.log(data.toString().split("\n"));
     });
 });
+
 /*
  * run: redis-cli -h 127.0.0.1 -p 6380 ping
  * or: nc 127.0.0.1 6380
  * or: printf "PING\r\n" | nc -w 1 127.0.0.1 6380
  * or: telnet 127.0.0.1 6380
+ * I prefere to run: printf "PING\r\nPING\r\n" | nc -w 1 127.0.0.1 6380
  */
 server.listen(6380, "127.0.0.1");
