e08d999dec5c3f92abc2be2fc5601278920a56e7
Mohammad El-Shennawy <mohamedwork216@gmail.com>
2026-08-04 22:16:39 +0300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -3,7 +3,9 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
-  // Handle connection
+    connection.on("data", (data) => {
+        connection.write("+PING\r\n");
+    });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(6380, "127.0.0.1");
