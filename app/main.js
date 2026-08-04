784d74d99f9d95df2370c66b41fa4c45c25b102f
Mohammad El-Shennawy <mohamedwork216@gmail.com>
2026-08-04 23:44:38 +0300

    change port to a free port

--- app/main.js
+++ app/main.js
@@ -5,7 +5,13 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
     connection.on("data", (data) => {
         connection.write("+PONG\r\n");
+        console.log(data.toString().split("\n"));
     });
 });
-
-server.listen(6379, "127.0.0.1");
+/*
+ * run: redis-cli -h 127.0.0.1 -p 6380 ping
+ * or: nc 127.0.0.1 6380
+ * or: printf "PING\r\n" | nc -w 1 127.0.0.1 6380
+ * or: telnet 127.0.0.1 6380
+ */
+server.listen(6380, "127.0.0.1");
